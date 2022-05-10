7505434113ac9378ea343d94286c156e0099f0f9
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2022-05-10 21:17:32 +0200

    resolved

--- WebApplication/WebApplication/Controllers/StudentsController.cs
+++ WebApplication/WebApplication/Controllers/StudentsController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using WebApplication.Models;
+using WebApplication.Services;
 
 namespace WebApplication.Controllers
 {
@@ -7,10 +11,55 @@ namespace WebApplication.Controllers
     [ApiController]
     public class StudentsController : ControllerBase
     {
+        public List<Student> _student = new List<Student>();
+
         [HttpGet]
-        public string GetStudents()
+        public IActionResult GetStudents()
         {
-            return "Ala, Ola, Marcin";
+            CSVService.ReadCSV(_student);
+            return Ok(_student);
+        }
+
+        [HttpGet("{indexNumber}")]
+        public IActionResult GetStudent(string indexNumber)
+        {
+            CSVService.findStudent(_student, indexNumber);
+            if (_student.Count == 0)
+            {
+                return NoContent();
+            }
+            else 
+            {
+                return Ok(_student);
+            }
+        }
+
+        [HttpPut("{indexNumber}")]
+        public IActionResult updateStudent(string indexNumber, [FromBody] Student student)
+        {
+            _student = CSVService.updateStudent(student, indexNumber);
+            CSVService.SaveToCSV(_student, true);
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public IActionResult createStudent(Student student)
+        {
+            if (!Regex.IsMatch(student.indexNumber, @"s[0-9]+")) 
+            { 
+                return BadRequest("The indexNumber has invalid format."); 
+            }
+            _student.Add(student);
+            CSVService.SaveToCSV(_student, false);
+            return Ok(student);
+        }
+
+        [HttpDelete]
+        public IActionResult deleteStudent(string indexNumber)
+        { 
+            _student = CSVService.deleteStudent(indexNumber);
+            CSVService.SaveToCSV(_student, true);
+            return Ok(_student);
         }
     }
 }
---
+++ WebApplication/WebApplication/Services/CSVService.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using WebApplication.Models;
+
+namespace WebApplication.Services
+{
+    public class CSVService
+    {
+        public static string pathToFile = "./Data/students.csv";
+
+        public static void ReadCSV(List<Student> list)
+        {
+            var lines = System.IO.File.ReadAllLines(pathToFile);
+            foreach (string line in lines)
+            {
+                var values = line.Split(',');
+                list.Add(new Student()
+                {
+                    FirstName = values[0],
+                    LastName = values[1],
+                    indexNumber = values[2],
+                    Birthdate = values[3],
+                    Studies = values[4],
+                    Mode = values[5],
+                    Email = values[6],
+                    FathersName = values[7],
+                    MothersName = values[8]
+                });
+            }
+        }
+
+        public static void findStudent(List<Student> list, string indexNumber) 
+        {
+            var lines = System.IO.File.ReadAllLines(pathToFile);
+            foreach (string line in lines)
+            {
+                var values = line.Split(',');
+                if(values[2].Equals(indexNumber))
+                {
+                    list.Add(new Student()
+                    {
+                        FirstName = values[0],
+                        LastName = values[1],
+                        indexNumber = values[2],
+                        Birthdate = values[3],
+                        Studies = values[4],
+                        Mode = values[5],
+                        Email = values[6],
+                        FathersName = values[7],
+                        MothersName = values[8]
+                    });
+                }
+            }
+        }
+
+        public static List<Student> updateStudent(Student student, string indexNumber)
+        {
+            List<Student> students = new List<Student>();
+            var lines = System.IO.File.ReadAllLines(pathToFile);
+            foreach (string line in lines)
+            {
+                var values = line.Split(',');
+                if (!values[2].Equals(indexNumber))
+                {
+                    students.Add(new Student()
+                    {
+                        FirstName = values[0],
+                        LastName = values[1],
+                        indexNumber = values[2],
+                        Birthdate = values[3],
+                        Studies = values[4],
+                        Mode = values[5],
+                        Email = values[6],
+                        FathersName = values[7],
+                        MothersName = values[8]
+                    });
+                }
+                else
+                {
+                    students.Add(student);
+                }
+            }
+            return students;
+        }
+
+        public static void SaveToCSV(List<Student> saveData, bool overwrite)
+        {
+            var lines = new List<string>();
+            IEnumerable<PropertyDescriptor> props = TypeDescriptor.GetProperties(typeof(Student)).OfType<PropertyDescriptor>();
+            var header = string.Join(",", props.ToList().Select(x => x.Name));
+            //lines.Add(header);
+            var valueLines = saveData.Select(row => string.Join(",", header.Split(',').Select(a => row.GetType().GetProperty(a).GetValue(row, null))));
+            lines.AddRange(valueLines);
+            if (overwrite == true)
+            {
+                File.WriteAllLines(pathToFile, lines.ToArray());
+            }
+            else
+            {
+                File.AppendAllLines(pathToFile, lines.ToArray());
+            }
+        }
+
+        public static List<Student> deleteStudent(string indexNumber)
+        {
+            List<Student> students = new List<Student>();
+            var lines = System.IO.File.ReadAllLines(pathToFile);
+            foreach (string line in lines)
+            {
+                var values = line.Split(',');
+                if (!values[2].Equals(indexNumber))
+                {
+                    students.Add(new Student()
+                    {
+                        FirstName = values[0],
+                        LastName = values[1],
+                        indexNumber = values[2],
+                        Birthdate = values[3],
+                        Studies = values[4],
+                        Mode = values[5],
+                        Email = values[6],
+                        FathersName = values[7],
+                        MothersName = values[8]
+                    });
+                }
+            }
+            return students;
+        }
+
+    }
+}
