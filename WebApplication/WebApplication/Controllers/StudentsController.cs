a7da773a7f2781782d5b48e92b36af2ffc6fe2a6
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2022-05-10 16:58:54 +0200

    add student class

---
+++ WebApplication/WebApplication/Controllers/StudentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents()
+        {
+            return "Ala, Ola, Marcin";
+        }
+    }
+}
