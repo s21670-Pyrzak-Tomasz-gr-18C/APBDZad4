a0bd2a3fe322c31e7688e35b1af7fa4f5612dbf9
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2022-06-06 23:47:11 +0200

    resolved

---
+++ WebApplication_Cw4/WebApplication_Cw4/Controllers/AnimalsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_Cw4.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        [HttpGet("animals")]
+        public IActionResult getAnimals()
+        {
+            return Ok();
+        }
+
+
+    }
+}
